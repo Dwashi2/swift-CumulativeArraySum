16cb228683f1f5dfd73a73d7e9d3b77329d82dd0
Daniel Washington <danielvertigo@hotmail.com>
2021-05-31 14:40:16 -0300

    Files

--- CumulativeArraySum/ViewController.swift
+++ CumulativeArraySum/ViewController.swift
@@ -5,6 +5,22 @@
 //  Created by Daniel Washington Ignacio on 31/05/21.
 //
 
+
+/*
+ Create a function that takes an array of numbers and returns an array where each number is the sum of itself + all previous numbers in the array.
+
+ Examples
+
+ cumulativeSum([1, 2, 3]) ➞ [1, 3, 6]
+
+ cumulativeSum([1, -2, 3]) ➞ [1, -1, 2]
+
+ cumulativeSum([3, 3, -2, 408, 3, 3]) ➞ [3, 6, 4, 412, 415, 418]
+ Notes
+
+ Return an empty array if the input is an empty array.
+ */
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -12,8 +28,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+      //  print(self.cumulativeSum([1, 2, 3]))
+       // print(self.cumulativeSum([1, -2, 3]))
+        print(self.cumulativeSum([3, 3, -2, 408, 3, 3]))
     }
 
+    func cumulativeSum(_ arr: [Int]) -> [Int] {
+        var sum: Int = 0
+        var resulArray: [Int] = []
+        for n in arr{
+            sum = sum + n
+            resulArray.append(sum)
+
+        }
+        
+        return resulArray
+    }
 
 }
 
